3b148fec2b629714c24d26a2df32a0b017d913e5
PLD908 <oluyededivine@gmail.com>
2025-12-11 02:49:20 +0100

    `Added tests for directorydata_service/users views`

--- directorydata_service/users/tests.py
+++ directorydata_service/users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+MAGIC_PASSWORD = "CSE270Rocks!"
+
+class BasicViewsTest(TestCase):
+    
+    def test_index_with_magic_password(self):
+        response = self.client.get(reverse('index'), {'password': MAGIC_PASSWORD})
+        self.assertEqual(response.status_code, 200)
+    
+    def test_index_with_admin_user(self):
+        response = self.client.get(reverse('index'), {'username': 'admin', 'password': 'qwerty'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_invalid_credentials(self):
+        response = self.client.get(reverse('index'), {'username': 'someone', 'password': 'wrong'})
+        self.assertEqual(response.status_code, 401)
+
+    def test_ingest_endpoint(self):
+        response = self.client.get(reverse('ingest'))
+        self.assertEqual(response.status_code, 200)
