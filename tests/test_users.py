f0d1e9e0663af81cbfebab5efb4d4a06dd716a12
PLD908 <oluyededivine@gmail.com>
2025-12-11 02:53:57 +0100

    `Refactored test_users.py to use Django's built-in testing framework`

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,30 +1,21 @@
-import requests
+from django.test import TestCase
+from django.urls import reverse
 
-BASE_URL = "http://127.0.0.1:8000/users/"
+class UserTests(TestCase):
+    def test_valid_admin_credentials(self):
+        """
+        Test 1:
+        Call /users/?username=admin&password=qwerty
+        Expect HTTP 200
+        """
+        response = self.client.get(reverse('index'), {'username': 'admin', 'password': 'qwerty'})
+        self.assertEqual(response.status_code, 200)
 
-def test_valid_admin_credentials():
-    """
-    Test 1:
-    Call /users/?username=admin&password=qwerty
-    Expect HTTP 200
-    """
-    params = {
-        "username": "admin",
-        "password": "qwerty"
-    }
-    response = requests.get(BASE_URL, params=params)
-    assert response.status_code == 200
-
-
-def test_invalid_admin_credentials():
-    """
-    Test 2:
-    Call /users/?username=admin&password=admin
-    Expect HTTP 401
-    """
-    params = {
-        "username": "admin",
-        "password": "admin"
-    }
-    response = requests.get(BASE_URL, params=params)
-    assert response.status_code == 401
+    def test_invalid_admin_credentials(self):
+        """
+        Test 2:
+        Call /users/?username=admin&password=admin
+        Expect HTTP 401
+        """
+        response = self.client.get(reverse('index'), {'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 401)
